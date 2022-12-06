f3919d4c20c20692e9df72f80fbd6f6376a8bfe9
Daniel Burrall <Danburrall@gmail.com>
2022-12-06 11:27:10 -0500

    finished Hash exercises

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -2,25 +2,40 @@ def create_favorite_hash(color, number)
   # return a hash with the following key/value pairs:
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
+
+  color ={
+    "green": "Favorite"
+  }
+  number= {
+    14: "Favroite number"
+  }
+
 end
 
 def favorite_color(favorite_list)
   # return the value of the color key
+  number[:14]
 end
 
 def favorite_number(favorite_list)
   # use #fetch to return the value of the number key or 42 if the key is not found
+
 end
 
 def update_favorite_movie(favorite_list, movie)
   # Step 1: add/update the key of movie (as a symbol)
+  movie = {
+    "Wolf of Wall street": "Love Leo",
+    "The Northman" : "Weird dark movie"
+  }
 
   # Step 2: return the hash (because Step 1 returns the value of the movie key)
-  favorite_list
+  favorite_list.each do |movie, type| puts "#{movie} #{type}"
 end
 
 def remove_favorite_number(favorite_list)
   # Step 1: delete the number data
+  number.delete(number)
 
   # Step 2: return the hash (because Step 1 returns the value of the number key)
   favorite_list
